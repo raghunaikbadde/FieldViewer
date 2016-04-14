5584eabc947b2f29b11bb5cedf0b5ebdb9e4ef97
tarun tiwari <tarun.tiwari@mastercard.com>
2016-04-14 11:50:30 +0530

    new screens updated.

--- JobViewer/src/com/lanesgroup/jobviewer/MainActivity.java
+++ JobViewer/src/com/lanesgroup/jobviewer/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.risk_assessment_screen);
+        setContentView(R.layout.confined_space_manhole_screen);
         
         /*Button button1 = (Button) findViewById(R.id.clock_in);
         button1.setOnClickListener(new OnClickListener() {
