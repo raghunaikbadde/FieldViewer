29524fbe91e414bc8d441f3876b84e7a70e382f3
Bihari Padhy, Rasa <Rasa_BihariPadhy@mastercard.com>
2016-05-01 19:18:31 +0530

    Added send assessment api in work with no photo.

--- JobViewer/src/com/jobviewer/nophotos/fragment/NoPhotosAssessmentCompleteFragment.java
+++ JobViewer/src/com/jobviewer/nophotos/fragment/NoPhotosAssessmentCompleteFragment.java
@@ -1,8 +1,12 @@
 package com.jobviewer.nophotos.fragment;
 
 import android.app.Fragment;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,19 +15,28 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.jobviewer.comms.CommsConstant;
 import com.jobviewer.db.objects.CheckOutObject;
+import com.jobviewer.exception.ExceptionHandler;
+import com.jobviewer.exception.VehicleException;
 import com.jobviewer.nophotos.WorkWithNoPhotosQuestionManager;
 import com.jobviewer.provider.JobViewerDBHandler;
+import com.jobviewer.survey.object.util.GsonConverter;
 import com.jobviewer.util.ActivityConstants;
 import com.jobviewer.util.Constants;
+import com.jobviewer.util.GPSTracker;
+import com.jobviewer.util.Utils;
+import com.jobviwer.response.object.User;
 import com.lanesgroup.jobviewer.ActivityPageActivity;
 import com.lanesgroup.jobviewer.R;
+import com.lanesgroup.jobviewer.fragment.ShoutOutActivity;
+import com.vehicle.communicator.HttpConnection;
 
 public class NoPhotosAssessmentCompleteFragment extends Fragment implements
 		OnClickListener {
 	private View mRootView;
 	private Button doneButton;
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -35,18 +48,18 @@ public class NoPhotosAssessmentCompleteFragment extends Fragment implements
 				.getCheckOutRemember(getActivity());
 		TextView screenTitle = (TextView) mRootView
 				.findViewById(R.id.screenTitle);
-		
+
 		TextView questionTitle = (TextView) mRootView
 				.findViewById(R.id.questionTitle);
-		questionTitle.setText(getResources().getString(R.string.persoal_risk_assement_complete_str));
-		TextView question = (TextView) mRootView
-				.findViewById(R.id.question);
-		questionTitle.setText(getResources().getString(R.string.persoal_riskassement_continue_str));
-		
-		TextView tapDone = (TextView) mRootView
-				.findViewById(R.id.tapDone);
+		questionTitle.setText(getResources().getString(
+				R.string.persoal_risk_assement_complete_str));
+		// TextView question = (TextView) mRootView.findViewById(R.id.question);
+		questionTitle.setText(getResources().getString(
+				R.string.persoal_riskassement_continue_str));
+
+		TextView tapDone = (TextView) mRootView.findViewById(R.id.tapDone);
 		tapDone.setVisibility(View.VISIBLE);
-		
+
 		if (ActivityConstants.EXCAVATION.equalsIgnoreCase(checkOutRemember
 				.getAssessmentSelected())) {
 			screenTitle.setText(getActivity().getResources().getString(
@@ -63,10 +76,88 @@ public class NoPhotosAssessmentCompleteFragment extends Fragment implements
 	@Override
 	public void onClick(View v) {
 		WorkWithNoPhotosQuestionManager.getInstance().saveAssessment("work");
-		Intent homeIntent = new Intent(getActivity(),
+		sendDataToServer();
+	}
+
+	private void sendDataToServer() {
+		if (Utils.isInternetAvailable(getActivity())) {
+			Utils.startProgress(getActivity());
+			ContentValues values = new ContentValues();
+			CheckOutObject checkOutRemember2 = JobViewerDBHandler
+					.getCheckOutRemember(getActivity());
+			User userProfile = JobViewerDBHandler.getUserProfile(getActivity());
+			if (checkOutRemember2 == null
+					|| Utils.isNullOrEmpty(checkOutRemember2.getWorkId())) {
+				values.put("work_id", "");
+			} else
+				values.put("work_id", checkOutRemember2.getWorkId());
+			values.put("survey_type", "Personal Risk Assessment");
+			values.put("related_type", "Work");
+			if (checkOutRemember2 == null
+					|| Utils.isNullOrEmpty(checkOutRemember2.getVistecId())) {
+				values.put("related_type_reference", "");
+			} else
+				values.put("related_type_reference",
+						checkOutRemember2.getVistecId());
+			values.put("started_at", ShoutOutActivity.getStartedAt());
+			values.put("completed_at", Utils.getCurrentDateAndTime());
+
+			values.put("created_by", userProfile.getEmail());
+			values.put("status", "Completed");
+			GPSTracker gpsTracker = new GPSTracker(getActivity());
+			values.put("location_latitude", gpsTracker.getLatitude());
+			values.put("location_longitude", gpsTracker.getLongitude());
+			String encodeToJsonString = GsonConverter.getInstance()
+					.encodeToJsonString(
+							WorkWithNoPhotosQuestionManager.getInstance()
+									.getQuestionMaster());
+			values.put("survey_json", encodeToJsonString);
+			Utils.SendHTTPRequest(getActivity(), CommsConstant.HOST
+					+ CommsConstant.SURVEY_STORE_API, values,
+					getSendSurveyHandler());
+		} else {
+			Intent homeIntent = new Intent(getActivity(),
 					ActivityPageActivity.class);
-		homeIntent.putExtra(Constants.WORK_NO_PHOTOS_HOME, true);
-		startActivity(homeIntent);
+			homeIntent.putExtra(Constants.WORK_NO_PHOTOS_HOME, true);
+			startActivity(homeIntent);
+			// saveInBackLog(obj);
+		}
+
+	}
+
+	private Handler getSendSurveyHandler() {
+		Handler handler = new Handler() {
+			@Override
+			public void handleMessage(Message msg) {
+
+				switch (msg.what) {
+				case HttpConnection.DID_SUCCEED:
+
+					String result = (String) msg.obj;
+					Log.i("Android", result);
+					Utils.StopProgress();
+					Intent homeIntent = new Intent(getActivity(),
+							ActivityPageActivity.class);
+					homeIntent.putExtra(Constants.WORK_NO_PHOTOS_HOME, true);
+					startActivity(homeIntent);
+					break;
+				case HttpConnection.DID_ERROR:
+					Utils.StopProgress();
+					String error = (String) msg.obj;
+					VehicleException exception = GsonConverter
+							.getInstance()
+							.decodeFromJsonString(error, VehicleException.class);
+					ExceptionHandler.showException(getActivity(), exception,
+							"Info");
+					break;
+
+				default:
+					break;
+				}
+
+			}
+		};
+		return handler;
 	}
 
 }
